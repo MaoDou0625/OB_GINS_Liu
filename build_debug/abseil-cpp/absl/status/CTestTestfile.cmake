48941416e1a16a6132f18826f72c8b7fdfe0dbd8
xunyi <liuziaoxunyi@gmail.com>
2025-09-04 08:50:38 +0800

    chore(gitignore): 忽略构建产物与VS/CMake生成文件；移除已跟踪的构建输出\n\n- ignore: bin/, lib/, build_debug/, build_release/, out/, .vs/, cmake-build-*/ 等\n- ignore: CMakeCache.txt, CMakeFiles/, VS中间文件(*.obj, *.pdb, *.tlog, *.ilk, *.log)\n- ignore: dataset/liu/ 以及 *.nav/*IMU_ERR* 输出文件\n- rm --cached: bin/, lib/, build_debug/, .vs/, dataset/liu/\n\n仅清理构建/输出文件，源码与数据输入不受影响。

--- build_debug/abseil-cpp/absl/status/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Code/OB_GINS/src/thirdparty/abseil-cpp/absl/status
-# Build directory: D:/Code/OB_GINS/build_debug/abseil-cpp/absl/status
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_debug/abseil-cpp/absl/utility/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Code/OB_GINS/src/thirdparty/abseil-cpp/absl/utility
-# Build directory: D:/Code/OB_GINS/build_debug/abseil-cpp/absl/utility
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
