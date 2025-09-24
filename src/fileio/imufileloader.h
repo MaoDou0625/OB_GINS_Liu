90c0f4f58784c50b913394a9cae8cb1a3cad717e
xunyi <liuziaoxunyi@gmail.com>
2025-09-24 20:05:43 +0800

    fix(debug): 解决 Debug 读取 IMU 问题
    - 按实际解析列数(n)拷贝与里程计处理，避免越界
    - 当列数 < 7 时使用名义采样间隔推进并置零增量，保证稳定
    - 兼容 8/9 列里程计格式，Debug/Release 下行为一致

--- src/fileio/imufileloader.h
+++ src/fileio/imufileloader.h
@@ -43,6 +43,18 @@ public:
 
         data_ = load();
 
+        // Debug 安全读取：按实际解析列数进行拷贝，防止越界
+        const size_t n = data_.size();
+        if (n < 7) {
+            // 列数不足，使用名义采样间隔推进时间并置零增量，避免崩溃
+            imu_.time   = imu_pre_.time + dt_;
+            imu_.dt     = dt_;
+            imu_.dtheta.setZero();
+            imu_.dvel.setZero();
+            imu_.odovel = 0.0;
+            return imu_;
+        }
+
         imu_.time = data_[0];
         memcpy(imu_.dtheta.data(), &data_[1], 3 * sizeof(double));
         memcpy(imu_.dvel.data(), &data_[4], 3 * sizeof(double));
@@ -54,11 +66,13 @@ public:
             imu_.dt = dt_;
         }
 
-        // 增量形式
-        if (columns_ == 8) {
-            imu_.odovel = data_[7] * imu_.dt;
-        } else if (columns_ == 9) {
+        // 增量形式（按实际列数判断 8/9 列）
+        if (n >= 9) {
             imu_.odovel = 0.5 * (data_[7] + data_[8]) * imu_.dt;
+        } else if (n >= 8) {
+            imu_.odovel = data_[7] * imu_.dt;
+        } else {
+            imu_.odovel = 0.0;
         }
 
         return imu_;
