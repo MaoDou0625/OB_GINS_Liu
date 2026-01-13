f58afbb2b7b38617a178d960c65b0fc5a0fd969b
MaoDou0625 <liuziaoxunyi@gmail.com>
2026-01-13 13:19:59 +0800

    Update build_with_vcpkg.bat to build both Debug and Release configurations

--- build_with_vcpkg.bat
+++ build_with_vcpkg.bat
@@ -1,4 +1,5 @@
 @echo off
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=D:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows
 cmake --build build --config Release
+cmake --build build --config Debug
 pause
